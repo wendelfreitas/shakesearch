2c3d9239c2d6a4260c9ec1a3338794882cb76ce0
Wendel Freitas <wendel.freitas@getinapp.com.br>
2022-05-13 12:16:33 -0300

    feat: create functions to read and organize txt file

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import path from 'path';
+import fs from 'fs';
+import { getContentsSanitized } from 'utils/helpers/get-contents-sanitized';
+import { getTitles } from 'utils/helpers/get-titles';
+import { getSonnets } from 'utils/helpers/get-sonnets';
 
-type Data = {
-  name: string;
-};
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    fs.readFile(
+      path.resolve(__dirname, '../../../../completeworks.txt'),
+      'utf8',
+      (err, data) => {
+        const lines = getContentsSanitized({ data });
+        const titles = getTitles({ lines });
+        const sonnets = getSonnets({ lines });
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' });
+        res.status(200).json({
+          titles,
+          sonnets,
+        });
+      }
+    );
+
+    // console.log(data);
+  } catch (err) {
+    console.error(err);
+
+    res.status(400).json({ error: 'Something went wrong' });
+  }
 }
---
+++ src/utils/helpers/get-contents-sanitized/index.ts
@@ -0,0 +1,20 @@
+type GetContentsSanitizedProps = {
+  /**
+   * The complete works string.
+   */
+  data: string;
+};
+
+/**
+ * Return the complete works string sanitized and in array.
+ */
+export const getContentsSanitized = ({ data }: GetContentsSanitizedProps) => {
+  const sanitized = data
+    .split(/\r?\n/)
+    .map((line) => line.trim())
+    .filter(Boolean);
+
+  const contents = sanitized.splice(sanitized.indexOf('Contents'));
+
+  return contents;
+};
