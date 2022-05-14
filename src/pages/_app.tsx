d7d581626dc05febed94c136e79ca17b47e2e16b
Wendel Freitas <wendel.freitas@getinapp.com.br>
2022-05-14 17:25:35 -0300

    chore: remove html component to fix bug

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,23 +4,20 @@ import Head from 'next/head';
 import theme from 'styles/theme';
 
 import type { AppProps } from 'next/app';
-import { Html } from 'next/document';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Html lang="en">
-        <Head>
-          <title>Shakesearch</title>
-          <meta
-            name="description"
-            content="Find anything about Shakespeare, including phrases, quotes, characters and sonettes."
-          />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <GlobalStyles />
-        <Component {...pageProps} />
-      </Html>
+      <Head>
+        <title>Shakesearch</title>
+        <meta
+          name="description"
+          content="Find anything about Shakespeare, including phrases, quotes, characters and sonettes."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
