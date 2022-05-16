16ff5c237c7eda482e3ac96b56120a429bac7146
Wendel Freitas <wendel.freitas@getinapp.com.br>
2022-05-16 05:21:59 -0300

    feat: create getCharacters helper function

---
+++ src/utils/helpers/get-characters/index.ts
@@ -0,0 +1,65 @@
+import { getWorks } from '../get-works';
+
+type GetCharactersProps = {
+  /**
+   * The complete works array of line strings.
+   */
+  lines: string[];
+};
+
+export type Character = {
+  id: number;
+  titleId: number;
+  name: string;
+  title: string;
+};
+
+export const getCharacters = ({ lines }: GetCharactersProps) => {
+  const works = getWorks({ lines });
+  let characters: Character[] = [];
+
+  works.forEach((work) => {
+    const personaes = work?.content
+      .map((line) => (line ? line : 'null'))
+      .splice(work?.content.indexOf('Dramatis Personæ') + 2);
+
+    const personas = personaes
+      ?.splice(0, personaes.indexOf('null'))
+      .map((character, index) => ({
+        id: index + 1,
+        index,
+        title: work.title
+          .toLowerCase()
+          .split(' ')
+          .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(' '),
+        titleId: work.id,
+        name: character
+          .toLowerCase()
+          .split(' ')
+          .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(' '),
+      }));
+
+    characters = [...characters, ...personas];
+  });
+
+  //   const characters = work?.content
+  //   .map((line) => (line ? line : 'null'))
+  //   .splice(work?.content.indexOf('Dramatis Personæ') + 2);
+
+  // console.log(
+  //   characters?.splice(0, characters.indexOf('null')).map((character) =>
+  //     character
+  //       .toLowerCase()
+  //       .split(' ')
+  //       .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+  //       .join(' ')
+  //   )
+  // );
+
+  return characters.map((character, index) => ({
+    ...character,
+    index,
+  }));
+};
