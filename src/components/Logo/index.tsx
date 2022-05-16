94bbddf2530950be1cf094f2eb14c273504a3dfd
Wendel Freitas <wendel.freitas@getinapp.com.br>
2022-05-15 21:11:06 -0300

    chore: add redirect to logo and change styles

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,17 +1,37 @@
+import { useRouter } from 'next/router';
 import * as S from './styles';
 
 export type LogoProps = {
   direction?: 'vertical' | 'horizontal';
 };
 
-export const Logo = ({ direction = 'vertical' }: LogoProps) => (
-  <S.Container direction={direction}>
-    <img
-      src="/img/hamlet.png"
-      alt="An Hamlet icon image"
-      width={300}
-      height={200}
-    />
-    <h1>Shakesearch</h1>
-  </S.Container>
-);
+export const Logo = ({ direction = 'vertical' }: LogoProps) => {
+  const router = useRouter();
+  const styles = {
+    vertical: {
+      width: 300,
+      height: 200,
+    },
+    horizontal: {
+      width: 350,
+      height: 100,
+    },
+  };
+
+  const handleRedirect = () => {
+    router.push('/');
+  };
+
+  return (
+    <S.Container direction={direction}>
+      <img
+        src="/img/hamlet.png"
+        alt="An Hamlet icon image"
+        width={styles[direction].width}
+        height={styles[direction].height}
+        onClick={handleRedirect}
+      />
+      <h1 onClick={handleRedirect}>Shakesearch</h1>
+    </S.Container>
+  );
+};
